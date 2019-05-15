7fdd8fe77d2ec31030905eb121091370d8437832
Yousuke Nakano <yousuke.computer@gmail.com>
2019-05-15 23:47:41 +0000

    ラケットを動かす処理を実装

--- brockout.js
+++ brockout.js
@@ -3,6 +3,7 @@
   //TODO いろいろ
 
   const ball = document.getElementById('ball');
+  const racket = document.getElementById('racket');
 
   let margin_top_value = 520;
   let vertically = -6;
@@ -16,6 +17,14 @@
 
     if (margin_top_value <= 0 ^ margin_top_value >= 520) vertically *= -1;
     if (margin_left_value < 0 ^ margin_left_value > 780) side *= -1;
+  }
+
+  let racket_margin_left = 337;
+  document.body.onkeydown = (event) => {
+    console.log(event);
+    if(event.key === 'h') racket_margin_left -= 15;
+    if(event.key === 'l') racket_margin_left += 15;
+    racket.style.marginLeft = racket_margin_left + 'px';
   }
 
   setInterval(move_ball, 40);
