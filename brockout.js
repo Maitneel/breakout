bcc5c21ad55054231b6aa3aab6b9d49ede3f07b7
Yousuke Nakano <yousuke.computer@gmail.com>
2019-05-15 21:49:36 +0000

    ボールが縦にだけ動く処理を実装

--- brockout.js
+++ brockout.js
@@ -1,6 +1,20 @@
-(function() {
+(function () {
   'use strict';
-  console.log('test');
   //TODO いろいろ
 
+  const ball = document.getElementById('ball');
+
+  let margin_top_value = 520;
+  let go_up = true;
+  function move_ball() {
+    ball.style.marginTop = margin_top_value + 'px';
+    //margin_top_value -= 2;
+    if (go_up) margin_top_value -= 6;
+    else margin_top_value += 6;
+
+    if (margin_top_value <= 0) go_up = false;
+    if (margin_top_value >= 520) go_up = true;
+  }
+
+  setInterval(move_ball, 40);
 })();
